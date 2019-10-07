5c9b585882628f9ad4aaca6dde9ac188b1d3cbf2
manuk <mahhov1@gmail.com>
2019-10-07 07:12:53 -0700

    first commit. cloned from js-player repo, added non-static clearChildren, added _ private indicator to some methods, and added readme

---
+++ readme.md
@@ -0,0 +1,77 @@
+# XElement
+
+Convenience class for creating custom HTML elements.
+
+## Toggle button example
+
+```html
+<style>
+	:host {
+		display: flex;
+	}
+
+	label {
+		display: flex;
+	}
+
+	input:checked ~ .toggle-off {
+		display: none
+	}
+
+	input:not(:checked) ~ .toggle-on {
+		display: none
+	}
+</style>
+
+<label>
+    <span id="title"></span>
+	<input id="checkbox" type="checkbox" hidden>
+	<slot name="toggle-on" class="toggle-on"></slot>
+	<slot name="toggle-off" class="toggle-off"></slot>
+</label>
+```
+
+```js
+const XElement = require('xx-element');
+const template = require('fs').readFileSync(`${__dirname}/toggle.html`, 'utf8');
+
+customElements.define('x-toggle', class extends XElement {
+	static get attributeTypes() {
+		return {title: false, checked: true};
+	}
+
+	static get htmlTemplate() {
+		return template;
+	}
+
+	connectedCallback() {
+		this.$('#checkbox').addEventListener('change', () => {
+			this.checked = this.$('#checkbox').checked;
+            this.dispatchEvent(new CustomEvent('change', {detail: this.checked}));
+		});
+		this.$('label').addEventListener('click', e => e.stopPropagation());
+	}
+
+	set title(value) {
+		this.$('#title').textContent = value;
+	}
+	
+	set checked(value) {
+		this.$('#checkbox').checked = value;
+	}
+});
+```
+
+## Usage
+
+- Create a class extending XElement.
+- Override the static getter methods `{...[attribute: isBoolean]} attributeTypes` and `string htmlTemplate`.
+- Add setter methods for each attribute.
+
+## Helpers
+
+The XElement base class provides 3 helper functions:
+
+- `$(selector)` invokes to `querySelector` on the XElement's `shadowRoot`.
+- `$$(selector)` invokes to `querySelectorAll` on the XElement's `shadowRoot`.
+- `clearChildren()` and `XElement.clearChildren(el)` clear the children elements of the XElement or an arbitrary element respectively.
---
+++ src/XElement.js
@@ -0,0 +1,78 @@
+class XElement extends HTMLElement {
+	static get attributeTypes() {
+		return {};
+		/* override */
+	}
+
+	static get htmlTemplate() {
+		return '';
+		/* override */
+	}
+
+	static get observedAttributes() {
+		return Object.keys(this.attributeTypes)
+			.map(propName => XElement.propToAttribName_(propName));
+	}
+
+	constructor() {
+		super();
+		this.attachShadow({mode: 'open'});
+		this.shadowRoot.innerHTML = this.constructor.htmlTemplate;
+
+		let properties = Object.getOwnPropertyDescriptors(Object.getPrototypeOf(this));
+		Object.entries(this.constructor.attributeTypes).forEach(([propName, boolean]) => {
+			let attribName = XElement.propToAttribName_(propName);
+			Object.defineProperty(this, XElement.setterName_(propName), properties[propName] || {set: () => 0});
+			Object.defineProperty(this, propName, boolean ? {
+				get: () => this.hasAttribute(attribName),
+				set: value => value ?
+					this.setAttribute(attribName, '') :
+					this.removeAttribute(attribName)
+			} : {
+				get: () => this.getAttribute(attribName),
+				set: value => this.setAttribute(attribName, value)
+			});
+		});
+
+	}
+
+	attributeChangedCallback(attribName, oldValue, newValue) {
+		let propName = XElement.attribToPropName_(attribName);
+		if (this.constructor.attributeTypes[propName])
+			this[XElement.setterName_(propName)] = this.hasAttribute(attribName);
+		else
+			this[XElement.setterName_(propName)] = newValue;
+	}
+
+	$(query) {
+		return this.shadowRoot.querySelector(query);
+	}
+
+	$$(query) {
+		return this.shadowRoot.querySelectorAll(query);
+	}
+
+	clearChildren() {
+		while (this.firstChild)
+			this.removeChild(this.firstChild);
+	}
+
+	static clearChildren(element) {
+		while (element.firstChild)
+			element.removeChild(element.firstChild);
+	}
+
+	static propToAttribName_(propName) {
+		return propName.replace(/[A-Z]/g, a => `-${a.toLowerCase()}`);
+	}
+
+	static attribToPropName_(attribName) {
+		return attribName.replace(/-(.)/g, (_, a) => a.toUpperCase());
+	}
+
+	static setterName_(propName) {
+		return `xel2_${propName}_`;
+	}
+}
+
+module.exports = XElement;
